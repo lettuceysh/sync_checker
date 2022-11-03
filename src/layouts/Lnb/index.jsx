95507d2a3d589346b0ffc36e507e1e058434f305
서세정 <seosejeong@seosejeong-ui-MacBookAir.local>
2022-11-03 22:36:28 +0900

    feat: 사이드메뉴 아이콘 추가

--- src/layouts/Lnb/index.jsx
+++ src/layouts/Lnb/index.jsx
@@ -5,6 +5,14 @@ import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+import NoteAltIcon from '@mui/icons-material/NoteAlt';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import SourceIcon from '@mui/icons-material/Source';
+import WorkIcon from '@mui/icons-material/Work';
+import FactCheckIcon from '@mui/icons-material/FactCheck';
 import Typography from '@mui/material/Typography';
 import ContentCut from '@mui/icons-material/ContentCut';
 import ContentCopy from '@mui/icons-material/ContentCopy';
@@ -15,7 +23,7 @@ import { colors } from '@/styles/colors';
 import { Link, useLocation } from 'react-router-dom';
 import { URL } from '@/const/router';
 import { useAlertStore } from '@/store';
-
+import AddLinkIcon from '@mui/icons-material/AddLink';
 const Lnb = () => {
   const location = useLocation();
   const { alert } = useAlertStore();
@@ -34,28 +42,60 @@ const Lnb = () => {
       <Title>Menu</Title>
       <MenuList>
         <CustomMenuItem className={getClass(URL.main)}>
-          <ListItemIcon>
-            <ContentCut fontSize="small" />
-          </ListItemIcon>
-          <StyledLink to={URL.main}>Dashboard</StyledLink>
+          <StyledLink to={URL.main}>
+            <ListItemIcon>
+              <DashboardIcon fontSize="small" />
+            </ListItemIcon>
+            Dashboard
+          </StyledLink>
         </CustomMenuItem>
         <CustomMenuItem className={getClass(URL.operationManagement)}>
-          <StyledLink to={URL.operationManagement}>Operation Management</StyledLink>
+          <StyledLink to={URL.operationManagement}>
+            <ListItemIcon>
+              <ApartmentIcon fontSize="small" />
+            </ListItemIcon>
+            Operation Management
+          </StyledLink>
         </CustomMenuItem>
         <CustomMenuItem className={getClass(URL.dataSourceManagement)}>
-          <StyledLink to={URL.dataSourceManagement}>DataSource Management</StyledLink>
+          <StyledLink to={URL.dataSourceManagement}>
+            <ListItemIcon>
+              <SourceIcon fontSize="small" />
+            </ListItemIcon>
+            DataSource Management
+          </StyledLink>
         </CustomMenuItem>
         <CustomMenuItem className={getClass(URL.projectManagement)}>
-          <StyledLink to={URL.projectManagement}>Project Management</StyledLink>
+          <StyledLink to={URL.projectManagement}>
+            <ListItemIcon>
+              <NoteAltIcon fontSize="small" />
+            </ListItemIcon>
+            Project Management
+          </StyledLink>
         </CustomMenuItem>
         <CustomMenuItem className={getClass(URL.configuration)}>
-          <StyledLink to={URL.configuration}>Job configuration</StyledLink>
+          <StyledLink to={URL.configuration}>
+            <ListItemIcon>
+              <WorkIcon fontSize="small" />
+            </ListItemIcon>
+            Job configuration
+          </StyledLink>
         </CustomMenuItem>
         <CustomMenuItem className={getClass(URL.checkResult)}>
-          <StyledLink to={URL.checkResult}>Check Result</StyledLink>
+          <StyledLink to={URL.checkResult}>
+            <ListItemIcon>
+              <AssignmentTurnedInIcon fontSize="small" />
+            </ListItemIcon>
+            Check Result
+          </StyledLink>
         </CustomMenuItem>
         <CustomMenuItem className={getClass(URL.userManagement)}>
-          <StyledLink to={URL.userManagement}>User Management</StyledLink>
+          <StyledLink to={URL.userManagement}>
+            <ListItemIcon>
+              <PeopleAltIcon fontSize="small" />
+            </ListItemIcon>
+            User Management
+          </StyledLink>
         </CustomMenuItem>
         <CustomMenuItem>
           <StyledLink
@@ -74,6 +114,9 @@ const Lnb = () => {
               });
             }}
           >
+            <ListItemIcon>
+              <FactCheckIcon fontSize="small" />
+            </ListItemIcon>
             Batch Validation Configuration
           </StyledLink>
         </CustomMenuItem>
@@ -105,6 +148,8 @@ const StyledLink = styled(Link)`
   width: 100%;
   font-size: 16px;
   padding: 8px 12px;
+  display: flex;
+  align-items: center;
   &.disable {
     color: ${colors.gray300};
   }
@@ -120,10 +165,13 @@ const CustomMenuItem = styled(MenuItem)`
   padding: 0;
   &.on {
     background-color: ${colors.white100};
-    margin: 0 3px;
+    /* margin: 0 3px; */
 
     a {
       color: ${colors.gray900};
+      svg {
+        fill: ${colors.gray900};
+      }
     }
   }
 `;
